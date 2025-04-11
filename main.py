8dd0a0083f9367191eae60d4163408355f12b644
03mattwaite <mwaite2@students.avc.edu>
2025-04-10 17:20:27 -0700

    L452 ATT1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 
 def main():
+    import random
     total = 0
     numbers = []
     """
@@ -9,7 +10,17 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    while total<= 100 :
+        random_number = random.randint(1,10)
+        numbers.append(random_number)
+        total += random_number
+    
+    last_random_num = numbers.pop()
+    
+    sum_excluding_last = total - last_random_num
+    
+    
+    print("Sum exlcluding last number:",sum_excluding_last)
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
